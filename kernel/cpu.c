d9ccf926ed6a8855ec5a20212b820a4ee613d516
John Dias <joaodias@google.com>
2016-08-12 08:36:08 -0700

    Silence some WLAN, PCIe, CPU and binder logging
    binder: set binder_debug_mask=0 to suppress logging
    
    Excessive logging -- not present on angler -- is affecting
    performance, contributing to missed audio deadlines and likely other
    latency-dependent tasks.
    Bug: 30375418
    
    Change-Id: I88b9c7fa4540ad46e564f44a0e589b5215e8487d
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Miguel Martinez <slverwolf@gmail.com>
    
    Silences WLAN, PCIe, and CPU suspend state kernel messages
    
    Bug: 27134656
    Change-Id: I681ec2171472514489365ca4bfc4ef16d9b344fe
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Miguel Martinez <slverwolf@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -573,8 +573,8 @@ int disable_nonboot_cpus(void)
 	 * with the userspace trying to use the CPU hotplug at the same time
 	 */
 	cpumask_clear(frozen_cpus);
+	pr_debug("Disabling non-boot CPUs ...\n");
 
-	pr_info("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
 			continue;
@@ -619,7 +619,7 @@ void __ref enable_nonboot_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	pr_info("Enabling non-boot CPUs ...\n");
+	pr_debug("Enabling non-boot CPUs ...\n");
 
 	arch_enable_nonboot_cpus_begin();
 
@@ -628,7 +628,7 @@ void __ref enable_nonboot_cpus(void)
 		error = _cpu_up(cpu, 1);
 		trace_suspend_resume(TPS("CPU_ON"), cpu, false);
 		if (!error) {
-			pr_info("CPU%d is up\n", cpu);
+			pr_debug("CPU%d is up\n", cpu);
 			cpu_device = get_cpu_device(cpu);
 			if (!cpu_device)
 				pr_err("%s: failed to get cpu%d device\n",
